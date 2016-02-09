32e5b9857638671241e89fffadb4c8aab1d9cdd6
RJN2 <roshan.narain@gmail.com>
2016-02-09 00:58:14 +0000

    add beerForm

---
+++ client/beerform.jsx
@@ -0,0 +1,45 @@
+BeerForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var numBeers = React.findDOMNode(this.refs.numBeers);
+		var beerDate = React.findDOMNode(this.refs.beerDate);
+
+		Meteor.call("insertBeer", numBeers.value, moment(beerDate.value).toDate(), function(e, r) {
+			if (e) alert(e.reason)
+		});
+		
+		numBeers.value = "";
+		beerDate.value = "";
+	}
+
+	render: function() {
+		return (
+			<div className="panel panel-default">
+				<div className="panel-heading">
+					<h3 className="panel-title">Beer Consumption</h3>
+				</div>
+				<div className="panel-body">
+					<form className="form-horizontal" onSubmit={this.handleSubmit}>
+						<div className="form-group">
+							<div className="col-sm-10">
+								<input type="number" className="form-control"
+									placeholder="How many beers?" ref="numBeers" />
+							</div>
+						</div>
+						<div className="form-group">
+							<div className="col-sm-10">
+								<input type="date" className="form-control" ref="beerDate" />
+							</div>
+						</div>
+
+						<div className="form-group">
+							<div className="col-sm-10">
+								<button type="submit" className="btn btn-primary btn-block">Add</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		);
+	}
+})
---
+++ collections.js
@@ -0,0 +1,18 @@
+Beers = new Meteor.Collection("Beers");
+
+Meteor.methods({
+	"insertBeer": function(numBeers, date) {
+		numBeers = parseInt(numBeers);
+
+		check(numBeers, Number);
+		check(date, Date);
+
+		return Beers.insert({beers: numBeers;date: date});
+	}
+
+	"removeBeer": function(id) {
+		check(id, String);
+		return Beers.remove(id);
+	}
+});
+
